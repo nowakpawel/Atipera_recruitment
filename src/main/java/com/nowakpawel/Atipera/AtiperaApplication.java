e117339ca65e1c891e2910b4d4f4a50892f77db1
nowakpawel <kontakt.nowakpawel@gmail.com>
2025-07-22 02:37:56 +0200

    Initial commit

---
+++ src/main/java/com/nowakpawel/Atipera/AtiperaApplication.java
@@ -0,0 +1,13 @@
+package com.nowakpawel.Atipera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiperaApplication.class, args);
+	}
+
+}
