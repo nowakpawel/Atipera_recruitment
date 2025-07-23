52392a5eaea242f76706c08f5a5020d384609bbe
nowakpawel <kontakt.nowakpawel@gmail.com>
2025-07-23 09:43:41 +0200

    Fix test

--- src/test/java/com/nowakpawel/Atipera/GithubIntegrationTest.java
+++ src/test/java/com/nowakpawel/Atipera/GithubIntegrationTest.java
@@ -6,7 +6,10 @@ import com.nowakpawel.Atipera.retrofit.dto.OwnerDto;
 import com.nowakpawel.Atipera.retrofit.dto.RepositoriesResponseDto;
 import com.nowakpawel.Atipera.web.service.GithubClientService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,16 +26,18 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
-@SpringBootTest
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
 public class GithubIntegrationTest {
-    @MockitoBean
+    @Mock
     private GithubClient githubClient;
-    @Autowired
+    @InjectMocks
     private GithubClientService service;
 
     @Test
     void shouldFetchNonForkReposAndBranchesSuccessfully() throws IOException {
-        String user = "nowakpawel";
+        final String USER = "nowakpawel";
+        String query = "user:" + USER;
         GitRepoDto repository = new GitRepoDto();
         repository.setName("Test repository");
         repository.setOwner(new OwnerDto("nowakpawel"));
@@ -45,9 +50,9 @@ public class GithubIntegrationTest {
         Call<RepositoriesResponseDto> mockCall = mock(Call.class);
 
         when(mockCall.execute()).thenReturn(Response.success(mockResponse));
-        when(githubClient.repositoriesForUser(user)).thenReturn(mockCall);
+        when(githubClient.repositoriesForUser(query)).thenReturn(mockCall);
 
-        ResponseEntity<RepositoriesResponseDto> response = service.getRepositories(user);
+        ResponseEntity<RepositoriesResponseDto> response = service.getRepositories(USER);
         assertTrue(response.getStatusCode().is2xxSuccessful());
     }
 }
